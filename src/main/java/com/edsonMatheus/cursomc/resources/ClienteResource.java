6e643bfd925d739094ee22c4d86594e5fd708913
Edson Matheus <edsonmaatheus@gmail.com>
2020-12-27 23:23:09 -0300

    Criando Profile de teste

--- src/main/java/com/edsonMatheus/cursomc/resources/ClienteResource.java
+++ src/main/java/com/edsonMatheus/cursomc/resources/ClienteResource.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/cliente")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
 
     @Autowired
